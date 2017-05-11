7a8679ffdb65ba0e1e0abacaa0420823ababbba3
Bernat Ferragut <elbernat@gmail.com>
2017-05-11 09:54:21 -0300

    Ts Types

--- Ts101.ts
+++ Ts101.ts
@@ -1,7 +1,16 @@
+// Primitives types
+//1.numbers
+let x: number = 2;
+let y: number = 3.2;
+//2.string
+let text: string = 'Hello';
+//3.boolean
+let havingMoney: boolean = true;
+//4.undefined 
+let myMoney = undefined; // value working with variables
+//5.null
+let myCat = null; // object working with objects
 
-let hello: string = 'Hello world!';
-document.body.innerHTML = hello;
-console.log(hello);
 
-
+ 
 
